88a1bb8535926be993ce3ad1d0968b4a659738ef
Sahil Dutta <123102778+Aqua-16@users.noreply.github.com>
2023-10-14 23:14:56 +0530

    Refactor comments

--- app/security_system.py
+++ app/security_system.py
@@ -8,7 +8,9 @@ class SecuritySystem:
     model_path = None
     cascade_path = None
     def __init__(self, algorithm='LBPH', model_path=None, cascade_path=None):
+        
         # Initialize face recognition model based on the specified algorithm
+        
         self.algorithm = algorithm
         if self.algorithm == 'LBPH':
             self.face_recognizer = cv2.face_LBPHFaceRecognizer.create()
@@ -19,8 +21,8 @@ class SecuritySystem:
         else:
             raise ValueError("Invalid algorithm specified")
 
-        # TODO 1
         # Load the trained face recognition model
+        
         if(model_path is None or not(os.path.exists(model_path))):
             self.model_path = "app/trained_models/trained_face_model_LBPH.xml"
         else:
@@ -28,21 +30,21 @@ class SecuritySystem:
 
         self.face_recognizer.read(self.model_path)
         
-        # Specify the path to the custom Haar Cascade classifier
         if(cascade_path is None or not(os.path.exists(cascade_path))):
             self.cascade_path = "app/haar_face.xml"
         else:
             self.cascade_path = cascade_path
+            
         # Load authorized persons from CSV
+        
         self.authorized_persons = self.load_authorized_persons()
+        
         # Load label-to-name mapping from CSV
+        
         self.label_to_name = self.load_label_to_name(algorithm)
-        # TODO 2
+        
         # Configure logging
-        # Logs are to be made to the access_logs.log file
-        # Set the logging level to INFO
-        # Use the following format: '%(asctime)s - %(message)s'
-        # Your code goes here
+        
         logging.basicConfig(
             filename='app/access_logs.log',
             level=logging.INFO,
@@ -51,9 +53,8 @@ class SecuritySystem:
 
     def load_authorized_persons(self):
         authorized_persons = {}
-            # TODO 1
-            # Load authorized persons from CSV
-            # Your code goes here
+        
+        # Load authorized persons from CSV
         with open("app/authorized_persons.csv") as file:
             reader = csv.reader(file)
             for row in reader:
@@ -63,9 +64,8 @@ class SecuritySystem:
     
     def load_label_to_name(self, algorithm):
         label_to_name = {}
-            # TODO 1
-            # Load label-to-name mapping from CSV
-            # Your code goes here
+
+        # Load label-to-name mapping from CSV
         with open("app/label_to_name.csv") as file:
             reader = csv.reader(file)
             for row in reader:
@@ -80,13 +80,11 @@ class SecuritySystem:
         # Convert the frame to grayscale for face detection
         gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-        # Load the custom Haar Cascade classifier for face detection
         face_cascade = cv2.CascadeClassifier(self.cascade_path)
 
-        # TODO 3
         # Perform face detection
         faces = face_cascade.detectMultiScale(gray_frame, scaleFactor = 1.2, minNeighbors = 5, minSize = (30,30))
-        # Iterate through each detected face
+
         # Initializing values in case no faces are detected
         person_name = "Unknown"
         authorization_status = False
@@ -121,6 +119,7 @@ class SecuritySystem:
             cv2.line(frame,(x,Y),(x+offset,Y),color,thickness)
             cv2.line(frame,(X-offset,Y),(X,Y),color,thickness)
             cv2.line(frame,(X,Y-offset),(X,Y),color,thickness)
+            
             # Annotate the frame with the recognized user's name and authorization status
            
             cv2.rectangle(frame, [x,y-35,200,30], color,-1)
@@ -131,10 +130,9 @@ class SecuritySystem:
         return person_name, authorization_status , frame
 
     def get_person_name(self, label, confidence):
-        # TODO 1
+        
             # Return the name of the person based on the label
-            # If the confidence is less than 80, return 'Unknown'
-            # Your code goes here
+            
             if(confidence<80 or (label not in self.label_to_name)):
                 return "Unknown"
             else:
@@ -144,15 +142,12 @@ class SecuritySystem:
         return self.authorized_persons.get(person_name, False)
 
     def log_access_attempt(self, person_name, is_authorized):
-        # TODO 2
-        # Log access attempt in the access_logs.log file
-        # Logs should be in the following format:
-        # <timestamp> - Person: <person_name>, Authorized: <is_authorized>
-        # Your code goes here
-
+        
+        # Log access attempt
+        
         logging.info(f'Person: {person_name}, Authorized: {is_authorized}')
         
-        # If unauthorized access, send a notification (you need to implement this)
+        # If unauthorized access, send a notification
         if not is_authorized:
             self.send_firebase_notification(person_name)
 
